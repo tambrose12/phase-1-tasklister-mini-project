be9902ce06f8cc9eb9f07f35de1ab4fe9d006b58
Taylor Ambrose <taykesby@gmail.com>
2023-02-01 11:44:24 -0500

    Task list JavaScript written.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+const button = document.getElementById('button');
+
+const uList = document.getElementById('tasks');
+
+const form = document.getElementById('create-task-form')
+
+function addTaskToList(eventObj) {
+  eventObj.preventDefault();
+  const listItem = document.createElement('li');
+  const newText = eventObj.target['new-task-description'].value
+  listItem.textContent = newText
+  uList.append(listItem)
+
+}
+
+form.addEventListener('submit', addTaskToList);
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
 });
+
+
+
